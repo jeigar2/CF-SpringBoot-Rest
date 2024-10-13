995a1d65e377e32b348c1347124030cb95f4a917
Jesus García <jeigar2@gmail.com>
2024-10-13 05:05:13 +0200

    Cambiado el repository por el service, y eliminado comentarios

--- src/main/java/es/comepiedras/retosprogramacion/config/LoadDatabase.java
+++ src/main/java/es/comepiedras/retosprogramacion/config/LoadDatabase.java
@@ -1,8 +1,7 @@
 package es.comepiedras.retosprogramacion.config;
 
 import es.comepiedras.retosprogramacion.model.Empleado;
-import es.comepiedras.retosprogramacion.repository.EmpleadoRepository;
-//import es.comepiedras.retosprogramacion.service.EmpleadoService;
+import es.comepiedras.retosprogramacion.service.EmpleadoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -14,19 +13,15 @@ public class LoadDatabase {
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
     @Bean
-    CommandLineRunner initDatabese(EmpleadoRepository empleadoRepository){
-//    CommandLineRunner initDatabese(EmpleadoService empleadoService){
+    CommandLineRunner initDatabese(EmpleadoService empleadoService){
         return args -> {
             Empleado javier = new Empleado("Javier", "CTO");
             Empleado blanca = new Empleado("Blanca", "Project Manager");
             Empleado juan = new Empleado("Juan", "CEO");
 
-//            logger.info("Carga inicial: {}, empleadoService.save(javier");
-//            logger.info("Carga inicial: {}, empleadoService.save(blanca");
-//            logger.info("Carga inicial: {}, empleadoService.save(juan");
-            log.info("Carga inicial: {}", empleadoRepository.save(javier));
-            log.info("Carga inicial: {}", empleadoRepository.save(blanca));
-            log.info("Carga inicial: {}", empleadoRepository.save(juan));
+            log.info("Carga inicial: {}", empleadoService.save(javier));
+            log.info("Carga inicial: {}", empleadoService.save(blanca));
+            log.info("Carga inicial: {}", empleadoService.save(juan));
         };
     }
 }
