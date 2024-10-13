9094f66045a5bf2b7eaefe6e41440b2686bb21b3
Jesus García <jeigar2@gmail.com>
2024-10-13 05:52:53 +0200

    Modificada las salidas de los endpoint para que sean ResponseEntity

--- src/main/java/es/comepiedras/retosprogramacion/controller/EmpleadoController.java
+++ src/main/java/es/comepiedras/retosprogramacion/controller/EmpleadoController.java
@@ -4,8 +4,11 @@ import es.comepiedras.retosprogramacion.model.Empleado;
 import es.comepiedras.retosprogramacion.service.EmpleadoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -19,38 +22,53 @@ public class EmpleadoController {
     }
 
     @GetMapping("/api/empleados")
-    public List<Empleado> getAllEmpleado(){
-        return empleadoService.findAll();
+    public ResponseEntity<List<Empleado>> getAllEmpleado(){
+        return ResponseEntity.ok(empleadoService.findAll());
     }
 
     @GetMapping("/api/empleados/{id}")
-    public Empleado getEmpleado(@PathVariable Long id){
-        return empleadoService.findById(id);
+    public ResponseEntity<Empleado> getEmpleado(@PathVariable Long id){
+        try {
+            Empleado empleado = empleadoService.findById(id);
+            return ResponseEntity.ok(empleado);
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @PostMapping("/api/empleados")
-    public Empleado createEmpleado(@RequestBody Empleado empleado){
-        return empleadoService.save(empleado);
+    public ResponseEntity<Empleado> createEmpleado(@RequestBody Empleado empleado,
+                                                   UriComponentsBuilder ucb){
+        Empleado empleadoGuardado = empleadoService.save(empleado);
+        URI uriEmpleado = ucb.path("/api/empleados/{id}").buildAndExpand(empleadoGuardado.getId()).toUri();
+        return ResponseEntity.created(uriEmpleado).build();
     }
 
     @PutMapping("/api/empleados/{id}")
-    public Empleado updateEmpleado(@PathVariable Long id,
-                                   @RequestBody Empleado empleado){
+    public ResponseEntity<Empleado> updateEmpleado(@PathVariable Long id,
+                                                   @RequestBody Empleado empleado
+//                                                   .UriComponentsBuilder ucb
+    ){
         try {
             Empleado aux = empleadoService.findById(id);
-            log.info("El empleado ha sido encontrado: " + aux);
+            log.info("El empleado ha sido encontrado: {}", aux);
             aux.setNombre(empleado.getNombre());
             aux.setPuesto(empleado.getPuesto());
-            log.info("Actualizado: " + aux);
-            return empleadoService.save(aux);
+            log.info("Actualizado: {}", aux);
+            empleadoService.save(aux);
+            return ResponseEntity.ok(aux);
         } catch (RuntimeException e) {
             log.warn("el empleado no se encuentras, se guarda como nuevo: {}", empleado);
-            return empleadoService.save(empleado);
+            Empleado empleadoGuardado = empleadoService.save(empleado);
+//            URI uriEmpleado = ucb.path("/api/empleados/{id}").buildAndExpand(empleadoGuardado.getId()).toUri();
+//            return ResponseEntity.created(uriEmpleado).build();
+            return ResponseEntity.ok(empleadoGuardado);
         }
     }
 
     @DeleteMapping("/api/empleados/{id}")
-    public void deleteEmpleado(@PathVariable Long id){
+    public ResponseEntity<Empleado> deleteEmpleado(@PathVariable Long id){
         empleadoService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
